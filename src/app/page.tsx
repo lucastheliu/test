47d2f1b70c8930cb02e8aae6963926b4c02676b1
LucasTL <lucasliuhaolin@gmail.com>
2024-10-04 18:13:29 +0200

    feat: add environment to canvas

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,38 @@
 "use client";
 
+import Link from "next/link";
+
 import * as THREE from "three";
 import { useRef, useState } from "react";
 
 import { Canvas, useFrame } from "@react-three/fiber";
-import { OrbitControls, useGLTF } from "@react-three/drei";
-
+import { OrbitControls, Environment, useGLTF } from "@react-three/drei";
 import { motion } from "framer-motion";
+
+function StyledLink({ value }: { value: string }) {
+  return (
+    <motion.div
+      className="cursor-pointer w-full"
+      whileHover={"hover"}
+      whileTap={{ scale: 1.1 }}
+    >
+      <motion.span className="relative">
+        <Link href={"/"}>{value}</Link>
+        <motion.span
+          className="absolute left-0 bottom-0 w-full h-px bg-zinc-800"
+          initial={{ scaleX: 0 }}
+          variants={{
+            hover: {
+              scaleX: 1,
+              originX: 0,
+              transition: { duration: 0.3 },
+            },
+          }}
+        />
+      </motion.span>
+    </motion.div>
+  );
+}
 
 interface ModelProps {
   url: string;
@@ -25,26 +51,59 @@ function Model({ url, start }: ModelProps) {
     }
   });
 
-  return <primitive ref={ref} object={scene} scale={[2.5, 2.5, 2.5]} />;
+  return <primitive ref={ref} object={scene} scale={[1, 1, 1]} />;
 }
 
 export default function Home() {
   const [isLoaded, setIsLoaded] = useState(false);
 
   return (
-    <main className="flex flex-col h-screen justify-center items-center">
-      <div className="w-5/12 mx-auto">
-        <h1 className="text-3xl font-bold border-b py-2">Hello, world!</h1>
-        <p className="text-lg py-4">
-          The development server is hosted at
-          <span className="font-mono rounded-md text-sm bg-zinc-200 mx-2 px-2 py-1">
-            localhost:3000
-          </span>
-        </p>
+    <main className="flex flex-col h-screen justify-between items-center">
+      <div className="grid grid-cols-2 place-items-center h-1/4 gap-x-12 w-2/3">
+        <div className="w-max flex flex-col justify-center mx-auto">
+          <h1 className="text-3xl font-bold border-b py-2">New Project</h1>
+          <div className="py-4">
+            <p className="flex gap-x-2 items-center text-lg">
+              Development server hosted at{" "}
+              <span className="font-mono rounded-md text-xs bg-zinc-100 px-2 py-1">
+                localhost:3000
+              </span>
+            </p>
+            <p className="text-sm py-2">
+              © 2024 Lucas Liu. All rights reserved.
+            </p>
+          </div>
+        </div>
+
+        <nav className="flex gap-x-12">
+          <div className="flex items-center gap-x-8">
+            <ul className="flex flex-col gap-y-1">
+              <h2 className="font-bold text-lg">Navigations</h2>
+              {["Home", "About", "Contacts"].map((value, index) => (
+                <li key={index}>
+                  <StyledLink value={value} />
+                </li>
+              ))}
+            </ul>
+          </div>
+
+          <div className="flex items-center gap-x-8">
+            <ul className="flex flex-col gap-y-1">
+              <h2 className="font-bold text-lg">Table of Contents</h2>
+              {["Getting Started", "Documentation", "Projects"].map(
+                (value, index) => (
+                  <li key={index}>
+                    <StyledLink value={value} />
+                  </li>
+                ),
+              )}
+            </ul>
+          </div>
+        </nav>
       </div>
 
       <motion.div
-        className="h-1/2 w-1/2 m-8"
+        className="h-3/4 w-full"
         initial={{ opacity: 0 }}
         animate={{ opacity: isLoaded ? 1 : 0 }}
         exit={{ opacity: 0 }}
@@ -53,8 +112,8 @@ export default function Home() {
         <Canvas onCreated={() => setIsLoaded(true)}>
           <ambientLight intensity={Math.PI / 3} />
           <directionalLight position={[5, 5, 5]} intensity={Math.PI} />
-          <directionalLight position={[-5, -5, -5]} intensity={Math.PI / 2} />
           <Model url="monkey.glb" start={isLoaded} />
+          <Environment preset="dawn" background />
           <OrbitControls />
         </Canvas>
       </motion.div>
